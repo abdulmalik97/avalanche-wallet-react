4941ff5d16fa6273ea52b3da63bb61f9ab6d244b
Malik Abd <mohammedabdmalik@gmail.com>
2021-09-22 03:19:51 -0500

    draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,172 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { useMoralis, MoralisProvider, useMoralisWeb3ApiCall, useMoralisWeb3Api } from "react-moralis";
+import { Button, ButtonGroup, Box, Wrap } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption } from "@chakra-ui/react";
+
+const LogoutButton = () => {
+  const { logout, isAuthenticating } = useMoralis();
+
+  return (
+    <Button colorScheme="red" variant="solid" isLoading={isAuthenticating} onClick={() => logout()} disabled={isAuthenticating}>
+      Logout
+    </Button>
+    // <button onClick={() => logout()} disabled={isAuthenticating}>
+    //   Authenticate
+    // </button>
+  );
+};
+
+const displayTokenBalancesTable = (tokenData) => {
+  return (
+    <div>
+      <Table variant="simple">
+        <TableCaption>Avalanche Wallet Assets</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Token Name</Th>
+            <Th>Balance</Th>
+            <Th>Symbol</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {tokenData.map((element) => {
+            return (
+              <React.Fragment>
+                <Tr>
+                  <Td>{element.name}</Td>
+                  <Td>{element.balance / ("1e" + element.decimals)}</Td>
+                  <Td>{element.symbol}</Td>
+                </Tr>
+              </React.Fragment>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </div>
+  );
+};
+
+const displayNFTBalancesTable = (NFTData) => {
+  return (
+    <Box d="flex" align-items="baseline">
+      <Table variant="simple">
+        <TableCaption> NFT Assets</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>NFT Name</Th>
+            <Th>Token Address</Th>
+            <Th>Token ID</Th>
+            <Th>Contract Type</Th>
+
+            {/* <Th>Symbol</Th> */}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {NFTData.result.map((element) => {
+            return (
+              <React.Fragment>
+                <Wrap>
+                  <Tr>
+                    <Td>{element.name}</Td>
+                    <Td>
+                      <Wrap>{element.token_address}</Wrap>
+                    </Td>
+                    <Td>
+                      <Wrap>{element.token_id}</Wrap>
+                    </Td>
+                    <Td>{element.contract_type}</Td>
+                  </Tr>
+                </Wrap>
+              </React.Fragment>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+};
 
 function App() {
+  const { authenticate, isAuthenticated, user } = useMoralis();
+  const Web3Api = useMoralisWeb3Api();
+  const { fetch, data, error, isLoading } = useMoralisWeb3ApiCall(Web3Api.account.getNativeBalance, {
+    chain: "avalanche",
+  });
+  const {
+    fetch: tokenFetch,
+    data: tokenData,
+    error: tokenError,
+    isLoading: tokenIsLoading,
+  } = useMoralisWeb3ApiCall(Web3Api.account.getTokenBalances, {
+    chain: "eth",
+  });
+
+  const {
+    fetch: nftFetch,
+    data: nftData,
+    error: nftError,
+    isLoading: nftLoading,
+  } = useMoralisWeb3ApiCall(Web3Api.account.getNFTs, {
+    chain: "avalanche",
+  });
+
+  if (!isAuthenticated) {
+    return (
+      <div>
+        <button onClick={() => authenticate()}>Authenticate</button>
+      </div>
+    );
+  }
+
+  // useEffect(() => {
+  //   // document.title = `You clicked ${count} times`;
+  // }, [tokenData]); // Only re-run the effect if count changes
+
+  const tokenDataResult = {};
+
   return (
-    <div className="App">
+    <Box className="App" d="flex" alignItems="center">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>Welcome {user.get("username")}</h1>
+          <LogoutButton />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!isLoading && data !== null ? (
+          <div>
+            <Button
+              colorScheme="green"
+              variant="outline"
+              // isLoading={isAuthenticating}
+              onClick={() => fetch()}
+              disabled={isLoading}
+            >
+              Refetch Native balance
+            </Button>
+
+            <pre>AVAX BALANCE : {JSON.stringify(data.balance, null, 2)}</pre>
+          </div>
+        ) : (
+          <p>Loading</p>
+        )}
+        {/* -------------Tokens------------ */}
+        <Button colorScheme="green" variant="outline" onClick={() => fetch()} disabled={tokenIsLoading}>
+          Refetch Tokens
+        </Button>
+        {!tokenIsLoading && tokenData !== null ? displayTokenBalancesTable(tokenData) : <p>Loading</p>}
+        {/* -------------NFT------------ */}
+        <Button colorScheme="green" variant="outline" onClick={() => nftfetch()} disabled={nftLoading}>
+          Refetch NFTs
+        </Button>
+        {!nftLoading && nftData !== null ? displayNFTBalancesTable(nftData) : <p>Loading</p>}
+
+        <div>
+          <a></a>
+        </div>
       </header>
-    </div>
+    </Box>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import Moralis from "moralis";
+import { useMoralis, MoralisProvider } from "react-moralis";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+  },
+});
+
+const moralisAppId = "77IfzDQc5dLFD3YbNKeuNrxHGe2c5OWGoMxgfLTU";
+const moralisServerURL = "https://ioc7ccqovdu5.grandmoralis.com:2053/server";
+// Moralis.initialize(moralisAppId);
+// Moralis.serverURL = moralisServerURL;
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={moralisAppId} serverUrl={moralisServerURL}>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </MoralisProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
